f909ef00823509af474f42b77cdefadbcc9650b8
leesoo <isoo7510@gmail.com>
2019-10-03 17:44:55 +0900

    add feature update data

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,7 @@ const {s3Multer} = require('../util/imgUploader');
 const {Post} = require('../models/Post');
 
 router.get('/', (req, res, next) => {
+    if (req.query.address) next();
     Post.findAll({raw: true})
         .then((result) => {
             res.json(result)
@@ -10,14 +11,31 @@ router.get('/', (req, res, next) => {
         .catch(next);
 });
 
+router.get('/', (req, res, next) => {
+    const {address} = req.query;
+    Post.findOne({where: {address}})
+        .then(result => res.json(result))
+        .catch(next);
+});
+
 router.post('/', s3Multer, (req, res, next) => {
     const {title, category, date, address, summary, text, lat, lng} = req.body;
     const img = req.files.map(e => e.location);
-    Post.create({title, category, date, address, summary, text, img, lat, lng})
+    Post.findOne({where: {address}})
         .then(post => {
-            res.status(200);
-            res.end();
-        }).catch(next);
+            post.update({title, category, date, address, summary, text, img, lat, lng})
+                .then(post => {
+                    res.status(200);
+                    res.end();
+                }).catch(next)
+        })
+        .catch(() => {
+            Post.create({title, category, date, address, summary, text, img, lat, lng})
+                .then(post => {
+                    res.status(200);
+                    res.end();
+                }).catch(next);
+        })
 });
 
 module.exports = router;
